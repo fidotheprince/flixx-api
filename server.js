cfe0c63a8d08d6c064307ff163245899f4c36e1d
Alvaro Castro Cid <alvarocastrocid@Alvaros-MacBook-Pro.local>
2024-01-21 07:38:54 -0500

    finished api;

--- server.js
+++ server.js
@@ -9,16 +9,25 @@ const fetch = require('node-fetch');
 
 const port = process.env.PORT || 3000;
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`); 
+});
+
+
 app.get('/resource/:type/:status', (req, res) => {
   
   const { type, status } = req.params; 
+
   const url = `${process.env.API_URL}${type}/${status}?api_key=${process.env.API_KEY}&language=en-US`
 
   fetch(url)
     .then(response => response.json())
-    .then(data => {
-        res.json(data)
-    })
+    .then(data => res.json(data))
     .catch(err => res.status(400).send('Error: ' + err));
 
 });
@@ -32,13 +41,19 @@ app.get('/search/:type', (req, res) => {
 
     fetch(url)
     .then(response => response.json())
-    .then(data => {
-        res.json(data)
-    })
+    .then(data => res.json(data))
     .catch(err => res.status(400).send('Error: ' + err));
 
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`); 
+app.get('/details/:type/:id', (req, res) => {
+
+    const { type, id } = req.params;
+    const url = `${process.env.API_URL}${type}/${id}?api_key=${process.env.API_KEY}&language=en-US`
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => res.json(data))
+    .catch(err => res.status(400).send('Error: ' + err));
+
 });
